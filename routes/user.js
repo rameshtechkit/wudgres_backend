0ed16914e2b1ff7f2790ed08822fb135d7e93d72
kaliguru <57206722+kaliguru@users.noreply.github.com>
2022-09-15 10:16:30 +0530

    chnaged

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ router.get("/usertest",(req,res)=>{
 
 router.post("/userposttest",(req,res)=>{
     const username=req.body.username;
+    console.log("Post is working")
     res.send("your username is:"+username);
 });
 
